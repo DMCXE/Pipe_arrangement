6e8401da8d2f647475b11f749166bca65e7469cb
DMCXE <1533210298@qq.com>
2022-08-29 19:30:41 +0800

    Pipe

--- testenv.py
+++ testenv.py
@@ -2,26 +2,33 @@ from PipeArrange import Pip_arrangement
 import numpy as np
 import time
 
-s1 = 3.2
-s2 = s1 * np.sqrt(3) / 2
-s3 = 2 * s1
-e = 0.1
-r = 1.1
-N = 1000
 
-'''
-s1 = 32 * 1e-3
-s2 = s1 * np.sqrt(3) / 2
-s3 = 2 * s1
-e = 32 * 1e-3
-r = 11  * 1e-3
-N = 3220
-'''
-st = time.time()
-pipe = Pip_arrangement(s1, s2, s3, e, r, N, 'Tri')
-pipe.arrangement()
-pos = pipe.Pippos
-print(pipe.R,pipe.PipeNum)
-stt = time.time()
-print(stt-st)
-pipe.visualize(pos, r)
+def test_visualize():
+    s1 = 3.2
+    s2 = s1 * np.sqrt(3) / 2
+    s3 = 2 * s1
+    e = 0.1
+    r = 1.1
+    N = 1000
+
+    '''
+    s1 = 32 * 1e-3
+    s2 = s1 * np.sqrt(3) / 2
+    s3 = 2 * s1
+    e = 32 * 1e-3
+    r = 11  * 1e-3
+    N = 3220
+    '''
+    st = time.time()
+    pipe = Pip_arrangement(s1, s2, s3, e, r, N, 'Tri')
+    pipe.arrangement()
+    pos = pipe.Pippos
+    stt = time.time()
+    pipe.visualize(pos, r)
+    return stt-st
+
+if __name__ == '__main__':
+    t = test_visualize()
+    print(t)
+
+
